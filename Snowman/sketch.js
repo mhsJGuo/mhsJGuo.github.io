8e24c255c86f0fa2fd33df8addaf434e72390605
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-03-08 08:59:32 -0500

    Add files via upload

---
+++ Snowman/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  // put setup code here
+createCanvas(400,400);
+// background Colour
+background(0,0,255)
+}
+
+function draw() {
+  // put drawing code here
+  //Ground
+  fill(0,255,0)
+  rect(0,300,400,100);
+
+  //Snowman Colour
+  fill(255,255,255);
+  //Bottom Circle
+  ellipse(200, 300, 150, 150);
+  //Middle circle
+  ellipse(200, 200, 100, 100);
+  //Top Circle
+  ellipse(200, 120, 75, 75);
+
+  //Left Arm
+  line(160,200,100,100);
+  //Right Arm
+  line(240,200,300,100);
+}
