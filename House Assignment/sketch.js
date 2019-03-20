78cb6f11c54cbc752b5956da7309c13a54671122
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-03-20 08:21:06 -0400

    Update sketch.js

--- House Assignment/sketch.js
+++ House Assignment/sketch.js
@@ -21,50 +21,50 @@ function draw() {
   stroke(0,0,0);
 
 //Ground
-  //Ground Colour (Green)
-  fill(0,255,0);
-  rect(0, height/2+houseWidth/2, width, height);
+    //Ground Colour (Green)
+    fill(0,255,0);
+    rect(0, height/2+houseWidth/2, width, height);
 
 //House
-    //Main part of the house
-      //House colour (Red)
-      fill(255,0,0);
-      rect(width/2, height/2, houseWidth, houseWidth/2);
+  //Main part of the house
+    //House colour (Red)
+    fill(255,0,0);
+    rect(width/2, height/2, houseWidth, houseWidth/2);
 
-    //Roof of house
-      //Roof Colour (Black)
-      fill(0,0,0);
-        triangle(width/2, height/2, width/2+houseWidth/2, height/2-houseWidth/2, width/2+houseWidth, height/2);
+  //Roof of house
+    //Roof Colour (Black)
+    fill(0,0,0);
+      triangle(width/2, height/2, width/2+houseWidth/2, height/2-houseWidth/2, width/2+houseWidth, height/2);
 
-    //Door
-      //Door Colour (White)
-      fill(255,255,255);
-      rect(width/2+houseWidth/7*3, height/2+houseWidth/4, houseWidth/7, houseWidth/4);
+  //Door
+    //Door Colour (White)
+    fill(255,255,255);
+    rect(width/2+houseWidth/7*3, height/2+houseWidth/4, houseWidth/7, houseWidth/4);
 
-    //Left window
-      //Left Window Colour (White)
-      fill(255,255,255);
-      rect(width/2+houseWidth/10, height/2+houseWidth/8, houseWidth/10*2, houseWidth/8);
+  //Left window
+    //Left Window Colour (White)
+    fill(255,255,255);
+    rect(width/2+houseWidth/10, height/2+houseWidth/8, houseWidth/10*2, houseWidth/8);
 
-    //Right window
-      //Right Window Colur (White)
-      fill(255,255,255);
-      rect(width/2+houseWidth/10*7, height/2+houseWidth/8, houseWidth/10*2, houseWidth/8);
+  //Right window
+    //Right Window Colur (White)
+    fill(255,255,255);
+    rect(width/2+houseWidth/10*7, height/2+houseWidth/8, houseWidth/10*2, houseWidth/8);
 
-    //Chimmney
-      //Chimmney Colour (Black)
-      fill(0,0,0);
-      rect(width/2,height/2-houseWidth/3,houseWidth/7,houseWidth/3);
+  //Chimmney
+    //Chimmney Colour (Black)
+    fill(0,0,0);
+    rect(width/2,height/2-houseWidth/3,houseWidth/7,houseWidth/3);
 
-    //Sun
-      //Sun Colour(Yellow)
-      fill(255,255,0);
-      ellipse(0,0, houseWidth);
+  //Sun
+    //Sun Colour(Yellow)
+    fill(255,255,0);
+    ellipse(0,0, houseWidth);
 
-    //Path To House
-      //Path Colour (Grey)
-      fill(100,100,100);
-      rect(width/2+houseWidth/7*3, height/2+houseWidth/2, houseWidth/7, houseWidth);
+  //Path To House
+    //Path Colour (Grey)
+    fill(100,100,100);
+    rect(width/2+houseWidth/7*3, height/2+houseWidth/2, houseWidth/7, houseWidth);
 
 //Garage
   //Main Part Of Garage
@@ -83,13 +83,13 @@ function draw() {
     rect(width/2-houseWidth/20, height/2+houseWidth/2, -houseWidth/2.5, -houseWidth/5);
 
 //Road
-      //Road Colour (Black)
-      fill(0,0,0);
-      rect(0, height/2+houseWidth/2*3, width, houseWidth);
+    //Road Colour (Black)
+    fill(0,0,0);
+    rect(0, height/2+houseWidth/2*3, width, houseWidth);
 
-    //Road Line
-      //Road Line Colour (Yellow)
-      stroke(255,255,0);
-      line(0, height/2+houseWidth*2, width, height/2+houseWidth*2);
+  //Road Line
+    //Road Line Colour (Yellow)
+    stroke(255,255,0);
+    line(0, height/2+houseWidth*2, width, height/2+houseWidth*2);
 
 }
