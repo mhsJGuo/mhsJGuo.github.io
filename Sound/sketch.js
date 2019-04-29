c8f107e5026db3e844d3f81fdfc92b60f5deb615
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-04-29 10:12:07 -0400

    Add files via upload

---
+++ Sound/sketch.js
@@ -0,0 +1,16 @@
+var image;
+
+function preload() {
+   image = loadImage("image.png")
+}
+function setup() {
+  // put setup code here
+ createCanvas(800, 600);
+ image(image,0,0,100,100)
+}
+
+function draw() {
+  //put drawing code here
+background(0);
+
+}
