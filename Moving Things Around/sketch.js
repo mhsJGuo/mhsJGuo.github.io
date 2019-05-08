8d732ae27fc19a1faa50012e7950d8d4ab8eca65
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-05-08 08:47:44 -0400

    Update sketch.js

--- Moving Things Around/sketch.js
+++ Moving Things Around/sketch.js
@@ -1,69 +1,62 @@
-let x= 200;
-let y= 200;
+let x = 200;
+let y = 200;
 
-let xSpeed= 5;
-let ySpeed= 5;
+let xSpeed = 5;
+let ySpeed = 5;
 
-let size= 50;
+let size = 50;
 
 let hit = false;
 
 function setup() {
   createCanvas(800, 600);
+  
   background(127.5);
-}
+}//End setup
 
 function draw() {
   background(127.5);
+  
   drawEllipsePink();
   drawRectBlue();
-
 
   hit = collideRectCircle(width/2,height/2,size*3,size*3,x,y,size,size);
 
   print(hit);
 
+  //What happens if the objects hit
   if (hit === true) {
     ySpeed=ySpeed*(-.5);
     xSpeed=xSpeed*(-.5);
 
-  }
-}
+  }//End if
+}//End draw
 
+//Code for the ellipse
 function drawEllipsePink() {
   fill(255,0,255);
   ellipse(x,y,size,size);
-
+  
+  //Code to set the speed
   x=x+xSpeed;
   y=y+ySpeed;
-
+  
+  //To make the ellipse bounce off the edges
   if (y>=height-size/2 || y<=0+size/2) {
     ySpeed= ySpeed*(-2);
-  }
+    
+  }//End if
+  
   if (x>=width-size/2 || x<=0+size/2) {
     xSpeed= xSpeed*(-2);
-  }
+    
+  }//End if
 
-}
+}//End function drawEllipsePink
 
-function drawEllipseRed() {
-  fill(255,0,0);
-  ellipse(x,y,size,size);
-
-  x=x+xSpeed;
-  y=y+ySpeed;
-
-  if (y>=height-size/2 || y<=0+size/2) {
-    ySpeed= ySpeed*(-1);
-  }
-  if (x>=width-size/2 || x<=0+size/2) {
-    xSpeed= xSpeed*(-1);
-  }
-
-}
-
+//Code for the rectangle
 function drawRectBlue() {
   fill(0,0,255);
   rect(width/2,height/2,size*3,size*3);
 
-}
+}//End function drawRectBlue
