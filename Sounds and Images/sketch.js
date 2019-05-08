5d097e17f56b444cbcec085b992cc7f12217c2ad
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-05-08 09:13:29 -0400

    Update sketch.js

--- Sounds and Images/sketch.js
+++ Sounds and Images/sketch.js
@@ -1,21 +1,29 @@
 let song;
 let image1;
 
+//To preload
 function preload() {
-   song = loadSound("bell.mp3")
-   image1 = loadImage("bell.jfif")
-}
+  song = loadSound("bell.mp3")
+  
+  image1 = loadImage("bell.jfif")
+   
+}//End function preload
+
 function setup() {
-  // put setup code here
  createCanvas(800,600);
-}
+   
+}//End function setup
 
 function draw() {
-  //put drawing code here
-background(0);
-image(image1,0,0,200,200)
-}
+   background(0);
+   
+   //Maing the image appear
+   image(image1,0,0,200,200)
+   
+}//End function draw
 
+//What happens when mouse is pressed
 function mousePressed() {
-  song.play();
-}
+   song.play();
+   
+}//End function mousePressed
