29806441132df71a81397ac3ae15cfc47e5286bb
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-05-08 08:50:14 -0400

    Update sketch.js

--- Using the Mouse 1/sketch.js
+++ Using the Mouse 1/sketch.js
@@ -1,44 +1,41 @@
-
 function setup() {
-  // put setup code here
-  //Creates a canvas
   createCanvas(800, 600);
+  
   background(127.5);
-}
-
-function draw() {
-  //put drawing code here
-
-}
+}//End function setup
 
 //Code used when the mouse is released
 function mouseReleased() {
   //Clears the canvas
   background(127.5);
+  
   //Colour and shapes for the target
   fill(0);
-  ellipse(mouseX, mouseY, 100, 100);
+    ellipse(mouseX, mouseY, 100, 100);
   fill(255);
-  ellipse(mouseX, mouseY, 50, 50);
+    ellipse(mouseX, mouseY, 50, 50);
   fill(0);
-  ellipse(mouseX, mouseY, 25, 25);
+   ellipse(mouseX, mouseY, 25, 25);
   fill(255);
-  ellipse(mouseX, mouseY, 12.5,12.5);
-}
+    ellipse(mouseX, mouseY, 12.5,12.5);
+  
+}//End function mouseReleased
 
 //Code used when the mouse is pressed
 function mousePressed() {
   //Clears the canvas
   background(127.5);
+  
   //Colour and shapes for the target
   fill(0);
-  ellipse(mouseX, mouseY, 200, 200);
+    ellipse(mouseX, mouseY, 200, 200);
   fill(255);
-  ellipse(mouseX, mouseY, 100, 100);
+    ellipse(mouseX, mouseY, 100, 100);
   fill(0);
-  ellipse(mouseX, mouseY, 50, 50);
+   ellipse(mouseX, mouseY, 50, 50);
   fill(255);
-  ellipse(mouseX, mouseY, 25, 25);
+   ellipse(mouseX, mouseY, 25, 25);
   fill(0);
-  ellipse(mouseX, mouseY, 12.5,12.5);
-}
+    ellipse(mouseX, mouseY, 12.5,12.5);
+  
+}//End function mousePressed
