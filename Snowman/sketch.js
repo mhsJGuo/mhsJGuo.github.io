2186608d92302b97cb919e887dca08e447ed2a26
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-05-08 08:42:47 -0400

    Update sketch.js

--- Snowman/sketch.js
+++ Snowman/sketch.js
@@ -1,34 +1,37 @@
 function setup() {
-  // put setup code here
-createCanvas(400,400);
-// background Colour
-background(0,0,255)
-}
+  createCanvas(400,400);
+  
+  background(0,0,255)
+}//End setup
 
 function draw() {
-  // put drawing code here
+    //Ground Colour
+  fill(0,255,0);
+  
+    //Ground
+  rect(0,300,400,100);
 
-  //Ground Colour
-fill(0,255,0);
-  //Ground
-rect(0,300,400,100);
+    //Snowman Colour
+  fill(255,255,255);
+  
+    //Bottom Circle
+  ellipse(200, 300, 150, 150);
+  
+    //Middle circle
+  ellipse(200, 200, 100, 100);
+  
+    //Top Circle
+  ellipse(200, 120, 75, 75);
 
-  //Snowman Colour
-fill(255,255,255);
-  //Bottom Circle
-ellipse(200, 300, 150, 150);
-  //Middle circle
-ellipse(200, 200, 100, 100);
-  //Top Circle
-ellipse(200, 120, 75, 75);
+    //Left Arm
+  line(160,200,100,100);
+  
+    //Right Arm
+  line(240,200,300,100);
 
-  //Left Arm
-line(160,200,100,100);
-  //Right Arm
-line(240,200,300,100);
-
-  //Sun Colour
-fill(255,255,0);
-  //Sun
-ellipse(50,50,50,50);
-}
+    //Sun Colour
+  fill(255,255,0);
+  
+    //Sun
+  ellipse(50,50,50,50);
+}//End draw
