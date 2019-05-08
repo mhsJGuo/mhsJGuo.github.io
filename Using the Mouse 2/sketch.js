3ec2e2cddcdfdb8ead5d38bd8f0f6f9d7365ff5a
mhsJGuo <48356550+mhsJGuo@users.noreply.github.com>
2019-05-08 08:52:31 -0400

    Update sketch.js

--- Using the Mouse 2/sketch.js
+++ Using the Mouse 2/sketch.js
@@ -1,30 +1,32 @@
-//Code should draw a black line when you press the left mouse button(pencil) and while draw a white line when you press the right mouse button(eraser)
+//Code should draw a black line when you press the left mouse button(pencil) and draw a white line when you press the right mouse button(eraser)
 function setup() {
-  // put setup code here
   createCanvas(800, 600);
+  
   background(255);
-
-}
+  
+}//End function setup
 
 function draw() {
-  //put drawing code here
   strokeWeight(10)
+  
   //What happens when the mouse is pressed
   if (mouseIsPressed === true) {
+    
     //What happens when the left mouse button is pressed
     if (mouseButton === LEFT) {
       strokeWeight(10)
       stroke(0);
+      
       line(mouseX, mouseY, pmouseX , pmouseY);
-    }
+    }//End if LEFT
 
     //What happens when the right mouse button is pressed
     else if (mouseButton === RIGHT) {
       strokeWeight(100);
       stroke(255);
+      
       line(mouseX, mouseY, pmouseX, pmouseY);
 
-    }
-  }
-
-}
+    }//End if RIGHT
+  }//End if TRUE
+}//End function draw
